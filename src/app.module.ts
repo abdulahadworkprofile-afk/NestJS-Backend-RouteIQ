850ce74c4a21d6a4ccb62925718986673c15d1ef
abdulahadworkprofile-afk <abdulahad.workprofile@gmail.com>
2025-10-24 12:05:14 -0700

    add database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: '192.168.100.35',
+      port: 1433,            
+      username: 'RouteIQUser',
+      password: 'password123',
+      database: 'routeIQ_2025',
+      entities: [],
+      synchronize: true,
+      options: {
+        encrypt: false,
+        trustServerCertificate: true,
+      },
+    }),
+  ],
 })
 export class AppModule {}
