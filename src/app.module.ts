a230ed66378d6db78aac07dfe227500aa771a3b0
abdulahadworkprofile-afk <abdulahad.workprofile@gmail.com>
2025-10-28 10:53:51 -0700

    sync

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,28 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { LoggerMiddleware } from './logger/logger.middleware';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mssql',
       host: '192.168.100.35',
-      port: 1433,            
+      port: 1433,
       username: 'RouteIQUser',
       password: 'password123',
       database: 'routeIQ_2025',
-      entities: [],
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
-      options: {
-        encrypt: false,
-        trustServerCertificate: true,
-      },
+      options: { encrypt: false, trustServerCertificate: true },
     }),
+    UsersModule,
   ],
+  providers: [UsersService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*'); 
+  }
+}
